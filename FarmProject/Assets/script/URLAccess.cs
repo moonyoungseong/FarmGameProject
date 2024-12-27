52d24888f8ac4b082706af7e9488df40bd5271f4
moonyoungseong <ansdudtjd123@naver.com>
2024-12-27 14:31:13 +0900

    웹페이지 접속 코드
    나중에 수정 필요

---
+++ FarmProject/Assets/script/URLAccess.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class URLAccess : MonoBehaviour
+{
+    public class URLAccess : MonoBehaviour
+    {
+        // ¿• ∆‰¿Ã¡ˆ ø≠±‚
+        public void OpenWebPage(string url)
+        {
+            Application.OpenURL(url);
+        }
+    }
+
+    //public void MainPage() // ¿Ã¿Â
+    //{
+    //    // ±Õ≥Û±Õ√Ã ªÁ¿Ã∆Æ
+    //    Application.OpenURL("https://www.greendaero.go.kr/");
+    //}
+
+    //public void CornPage()
+    //{
+    //    // ø¡ºˆºˆ ªÁ¿Ã∆Æ
+    //    Application.OpenURL("https://terms.naver.com/entry.naver?docId=1993362&cid=48180&categoryId=48247");
+    //}
+}
