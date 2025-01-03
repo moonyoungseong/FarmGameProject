966f9c9165d50afb4c7600d00a497d4e64b35188
moonyoungseong <ansdudtjd123@naver.com>
2025-01-03 18:36:44 +0900

    클래스명 변경
    상점뿐만 아니라 개수 관련 사용

---
+++ FarmProject/Assets/script/Inventory/CountManager.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class CountManager : MonoBehaviour
+{
+    void Start()
+    {
+        // InventoryManager가 데이터 로딩을 마친 후 이벤트를 통해 알림 받기
+        InventoryManager.Instance.OnDataLoaded += OnInventoryDataLoaded;
+    }
+
+    void OnInventoryDataLoaded()
+    {
+        // AllItemList 가져오기
+        List<Item> allItems = InventoryManager.Instance.AllItemList;
+
+        // MyItemList 가져오기
+        List<Item> myItems = InventoryManager.Instance.MyItemList;
+
+        // CurItemList 가져오기
+        List<Item> curItemList = InventoryManager.Instance.CurItemList;
+
+        // 특정 아이템 사용
+        foreach (var item in allItems)
+        {
+            //Debug.Log($"아이템 이름: {item.itemName}");
+        }
+    }
+}
