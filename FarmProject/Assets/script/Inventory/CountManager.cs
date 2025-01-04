0871d7733f2d1797f24f80e88a841da30a56b8ab
moonyoungseong <ansdudtjd123@naver.com>
2025-01-04 14:13:32 +0900

    거래창
    인덱스 정렬, 아이템 추가, 저장 등

--- FarmProject/Assets/script/Inventory/CountManager.cs
+++ FarmProject/Assets/script/Inventory/CountManager.cs
@@ -7,6 +7,7 @@ public class CountManager : MonoBehaviour
 {
     public TMP_InputField ItemNameInput, ItemNumberInput;
     private List<Item> myItems;
+    private List<Item> allItems;
 
     void Start()
     {
@@ -18,6 +19,8 @@ public class CountManager : MonoBehaviour
     {
         // MyItemList 가져오기
         myItems = InventoryManager.Instance.MyItemList;
+
+        allItems = InventoryManager.Instance.AllItemList;
 
         // 데이터 로딩 완료 후 작업
         Debug.Log("아이템 데이터가 로드되었습니다.");
@@ -36,11 +39,16 @@ public class CountManager : MonoBehaviour
         if (curItem != null)
         {
             Debug.Log($"아이템 이름: {curItem.itemName}");
+            curItem.quantity = (int.Parse(curItem.quantity) + int.Parse(ItemNumberInput.text)).ToString();
         }
         else
         {
             Debug.Log("아이템을 찾을 수 없습니다.");
+            // 전체에서 얻을 아이템을 찾아 내 아이템에 추가
+            Item curAllItem = allItems.Find(x => x.itemName == ItemNameInput.text);
+            if (curAllItem != null) myItems.Add(curAllItem);
         }
+        toEnd();
     }
 
     public void ReMoveClick()
@@ -55,13 +63,32 @@ public class CountManager : MonoBehaviour
         Item curItem = myItems.Find(x => x.itemName == ItemNameInput.text);
         if (curItem != null)
         {
-            myItems.Remove(curItem); // 아이템 제거
+            int curNumber = int.Parse(curItem.quantity) - int.Parse(ItemNumberInput.text);
+
+            if (curNumber <= 0) myItems.Remove(curItem); // 아이템 제거
+            else curItem.quantity = curNumber.ToString();
             Debug.Log($"아이템 {curItem.itemName}이(가) 제거되었습니다.");
         }
         else
         {
             Debug.Log("아이템을 찾을 수 없습니다.");
         }
+        toEnd();
+    }
+
+    public void toEnd() // 거래창 및 인벤 정리용
+    {
+        myItems.Sort((p1, p2) =>
+        {
+            int id1 = int.TryParse(p1.itemID, out int result1) ? result1 : int.MaxValue; // 숫자 변환 실패 시 최대값으로 처리
+            int id2 = int.TryParse(p2.itemID, out int result2) ? result2 : int.MaxValue;
+
+            return id1.CompareTo(id2);
+        });
+        //myItems.Sort((p1, p2) => p1.itemID.CompareTo(p2.itemID));
+        InventoryManager.Instance.Save();
+        ItemNameInput.text = "";
+        ItemNumberInput.text = "";
     }
 }
 
