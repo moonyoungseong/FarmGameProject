a7b8286060dc74d9318fbc434eca5d23a72cc1b8
moonyoungseong <ansdudtjd123@naver.com>
2025-01-17 18:45:05 +0900

    Update DecoManager.cs
    아이템 데이터 가져오기

--- FarmProject/Assets/script/HomeDeco/DecoManager.cs
+++ FarmProject/Assets/script/HomeDeco/DecoManager.cs
@@ -4,10 +4,25 @@ using UnityEngine;
 
 public class DecoManager : MonoBehaviour
 {
+    private List<Item> myItems;
+    private List<Item> allItems;
+    public List<Item> CurDecoList = new List<Item>(); // MyItemList이 Json으로 저장
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        // InventoryManager가 데이터 로딩을 마친 후 이벤트를 통해 알림 받기
+        InventoryManager.Instance.OnDataLoaded += OnInventoryDataLoaded;
+    }
+
+    void OnInventoryDataLoaded()
+    {
+        // MyItemList 가져오기
+        myItems = InventoryManager.Instance.MyItemList;
+        allItems = InventoryManager.Instance.AllItemList;
+
+        // 데이터 로딩 완료 후 작업
+        Debug.Log("꾸미기 데이터가 로드되었습니다.");
     }
 
     // Update is called once per frame
