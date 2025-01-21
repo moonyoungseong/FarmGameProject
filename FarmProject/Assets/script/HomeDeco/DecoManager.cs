918dfc6398cfb950cf8bfc0cdae68d3394f51a3e
moonyoungseong <ansdudtjd123@naver.com>
2025-01-21 09:48:00 +0900

    Update DecoManager.cs
    데코 버튼 클릭 시 DecoItems() 갱신

--- FarmProject/Assets/script/HomeDeco/DecoManager.cs
+++ FarmProject/Assets/script/HomeDeco/DecoManager.cs
@@ -11,10 +11,7 @@ public class DecoManager : MonoBehaviour
 
     void Start()
     {
-        // InventoryManager의 MyItemList에서 HomeDeco 아이템 필터링
-        DecoItems = InventoryManager.Instance.MyItemList.FindAll(item => item.itemType == "HomeDeco");
-
-        //Test();
+        UpdateDecoItems();
         HomeList();
     }
 
@@ -23,15 +20,13 @@ public class DecoManager : MonoBehaviour
         // 개수만큼 슬롯 보이기
         for (int i = 0; i < DecoSlot.Length; i++)
         {
-            //Slot[i].SetActive(i < CurItemList.Count);
-
             if (i < DecoItems.Count)
             {
                 // 슬롯에 아이템 이름과 아이콘 설정
                 Transform slotTransform = DecoSlot[i].transform;
                 slotTransform.Find("Name").GetComponent<TextMeshProUGUI>().text = DecoItems[i].itemName;
                 slotTransform.Find("Count").GetComponent<TextMeshProUGUI>().text = DecoItems[i].quantity + "개";
-                slotTransform.Find("Image").GetComponent<Image>().sprite = Resources.Load<Sprite>("Icons/" + DecoItems[i].itemIcon); // ItemIconImage는 슬롯 내의 Image 컴포넌트
+                slotTransform.Find("Image").GetComponent<Image>().sprite = Resources.Load<Sprite>("Icons/" + DecoItems[i].itemIcon);
                 DecoSlot[i].SetActive(true);
             }
             else
@@ -41,21 +36,16 @@ public class DecoManager : MonoBehaviour
         }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void UpdateDecoItems()
     {
-        
+        // InventoryManager의 MyItemList에서 HomeDeco 아이템 필터링
+        DecoItems = InventoryManager.Instance.MyItemList.FindAll(item => item.itemType == "HomeDeco");
     }
 
-    public void Test()
+    // 거래 이벤트 발생 시 호출
+    public void OnTradeCompleted()
     {
-        Debug.Log($"HomeDeco 아이템 개수: {DecoItems.Count}");
-
-        // HomeDeco 리스트 출력
-        foreach (var item in DecoItems)
-        {
-            Debug.Log($"아이템 이름: {item.itemName}, 아이템 ID: {item.itemID}");
-        }
+        UpdateDecoItems(); // DecoItems 갱신
+        HomeList();        // UI 업데이트
     }
-
 }
