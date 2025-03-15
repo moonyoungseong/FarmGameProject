512200b9722ed26caa20e10716b5aa533688cba7
moonyoungseong <ansdudtjd123@naver.com>
2025-03-15 14:52:19 +0900

    Update NaDataResetter.cs
    수정필요 - 이름은 변경되나 다시 로드 해야 하는 상황발생

--- FarmProject/Assets/script/Setting/NaDataResetter.cs
+++ FarmProject/Assets/script/Setting/NaDataResetter.cs
@@ -1,15 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class NaDataResetter : MonoBehaviour
 {
     //[SerializeField] private CharacterSelection characterSelection; // 이름 짓는 함수 있는 스크립트
+    public TMP_InputField ChangeNameInputField;  // TMP_InputField
 
     public void OnChangeNameButtonClicked()
     {
         // 싱글톤 인스턴스를 통해 접근
         //CharacterSelection.Instance.SaveCharacterName();
+
+        string chageName = ChangeNameInputField.text;  // 입력된 이름 가져오기
+        if (!string.IsNullOrEmpty(chageName))
+        {
+            PlayerPrefs.SetString("CharacterName", chageName);  // PlayerPrefs에 저장
+            PlayerPrefs.Save();  // 저장 강제 실행
+        }
+        else
+        {
+            Debug.LogWarning("Character name is empty!");
+        }
     }
 
     public void ResetAllData()
