72fb4f68d13a3ef63b0810bfa9b5042d8681bfe1
moonyoungseong <ansdudtjd123@naver.com>
2025-04-12 17:37:21 +0900

    Update QuestSlot.cs
    자물쇠 잠금 해제 및, 퀘스트 수락시 호춣하는 코드까지 메모

--- FarmProject/Assets/script/Quest/QuestSlot.cs
+++ FarmProject/Assets/script/Quest/QuestSlot.cs
@@ -6,9 +6,20 @@ using UnityEngine.EventSystems;
 public class QuestSlot : MonoBehaviour, IPointerEnterHandler
 {
     public Quest questData;  // 이 슬롯이 가진 퀘스트 데이터
+    public GameObject lockImage;  // Inspector에서 자물쇠 오브젝트 연결
 
     public void OnPointerEnter(PointerEventData eventData)
     {
         QuestDetailPanel.Instance.ShowDetails(questData);
     }
+
+    public void Unlock()    // 자물쇠 푸는 함수
+    {
+        if (lockImage != null)
+            lockImage.SetActive(false);
+    }
+
+    // 나중에 퀘스트 수락시 잠금 해재할때 쓸것 
+    // 자물쇠 해제
+    // FindObjectOfType<QuestListController>().UnlockQuestSlot(quest.questName);
 }
