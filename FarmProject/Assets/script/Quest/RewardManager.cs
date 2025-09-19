45835c59364cd910742c68a483ed7ef256eed855
moonyoungseong <ansdudtjd123@naver.com>
2025-09-19 22:02:32 +0900

    Update RewardManager.cs
    퀘스트 보상 로직 수정

--- FarmProject/Assets/script/Quest/RewardManager.cs
+++ FarmProject/Assets/script/Quest/RewardManager.cs
@@ -15,6 +15,23 @@ public class RewardManager : MonoBehaviour
         else Destroy(gameObject);
     }
 
+    //public void GiveReward(Reward reward)
+    //{
+    //    CountManager countManager = GameObject.FindObjectOfType<CountManager>();
+    //    if (countManager == null)
+    //    {
+    //        Debug.LogWarning("CountManager를 찾을 수 없습니다!");
+    //        return;
+    //    }
+
+    //    // 수량 1개 지급 (원하면 reward.quantity 사용 가능)
+    //    countManager.ItemNameInput.text = reward.itemname;
+    //    countManager.ItemNumberInput.text = "1";
+    //    countManager.GetItemClick();
+
+    //    Debug.Log($"보상 지급: 아이템 이름={reward.itemname}, 수량=1");
+    //}
+
     public void GiveReward(Reward reward)
     {
         CountManager countManager = GameObject.FindObjectOfType<CountManager>();
@@ -24,12 +41,10 @@ public class RewardManager : MonoBehaviour
             return;
         }
 
-        // 수량 1개 지급 (원하면 reward.quantity 사용 가능)
-        countManager.ItemNameInput.text = reward.itemname;
-        countManager.ItemNumberInput.text = "1";
-        countManager.GetItemClick();
+        int rewardQty = reward.quantity > 0 ? reward.quantity : 1;
+        countManager.GiveRewardItem(reward.itemname, rewardQty);
 
-        Debug.Log($"보상 지급: 아이템 이름={reward.itemname}, 수량=1");
+        Debug.Log($"보상 지급 완료: {reward.itemname}, 수량={rewardQty}");
     }
 
     public void GiveRewards(System.Collections.Generic.List<Reward> rewards)
