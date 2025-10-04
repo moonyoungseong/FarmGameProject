6b53182b2577e192edf4173bc30e8b47ac7451e1
moonyoungseong <ansdudtjd123@naver.com>
2025-10-04 12:57:06 +0900

    Update RewardManager.cs
    보상 로직 수정

--- FarmProject/Assets/script/Quest/RewardManager.cs
+++ FarmProject/Assets/script/Quest/RewardManager.cs
@@ -15,23 +15,6 @@ public class RewardManager : MonoBehaviour
         else Destroy(gameObject);
     }
 
-    //public void GiveReward(Reward reward)
-    //{
-    //    CountManager countManager = GameObject.FindObjectOfType<CountManager>();
-    //    if (countManager == null)
-    //    {
-    //        Debug.LogWarning("CountManager를 찾을 수 없습니다!");
-    //        return;
-    //    }
-
-    //    // 수량 1개 지급 (원하면 reward.quantity 사용 가능)
-    //    countManager.ItemNameInput.text = reward.itemname;
-    //    countManager.ItemNumberInput.text = "1";
-    //    countManager.GetItemClick();
-
-    //    Debug.Log($"보상 지급: 아이템 이름={reward.itemname}, 수량=1");
-    //}
-
     public void GiveReward(Reward reward)
     {
         CountManager countManager = GameObject.FindObjectOfType<CountManager>();
