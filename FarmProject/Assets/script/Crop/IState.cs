c2e5a1c814bd82adbf381b8cb1d569dddaf7d52d
moonyoungseong <ansdudtjd123@naver.com>
2025-11-28 13:48:17 +0900

    Delete IState.cs

--- FarmProject/Assets/script/Crop/IState.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public interface IState
-{
-    void HandleWatering(Crop crop); // 물 주기 처리
-    void HandleDisaster(Crop crop); // 자연재해 처리
-}
